47006ecc46ea9319c36fdd9bf538489690f4d750
ssummerhays <spencersummerhays3@gmail.com>
2025-02-13 21:47:32 -0700

    Updated PostStatus to fit in the MVP architecture

--- tweeter-web/src/components/postStatus/PostStatus.tsx
+++ tweeter-web/src/components/postStatus/PostStatus.tsx
@@ -1,8 +1,11 @@
 import "./PostStatus.css";
 import { useState } from "react";
-import { AuthToken, Status } from "tweeter-shared";
 import useToastListener from "../toaster/ToastListenerHook";
 import useUserInfo from "../userInfo/UserInfoHook";
+import {
+  PostStatusPresenter,
+  PostStatusView,
+} from "../../presenters/PostStatusPresenter";
 
 const PostStatus = () => {
   const { displayErrorMessage, displayInfoMessage, clearLastInfoMessage } =
@@ -10,39 +13,17 @@ const PostStatus = () => {
 
   const { currentUser, authToken } = useUserInfo();
   const [post, setPost] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
 
-  const submitPost = async (event: React.MouseEvent) => {
-    event.preventDefault();
-
-    try {
-      setIsLoading(true);
-      displayInfoMessage("Posting status...", 0);
-
-      const status = new Status(post, currentUser!, Date.now());
-
-      await postStatus(authToken!, status);
-
-      setPost("");
-      displayInfoMessage("Status posted!", 2000);
-    } catch (error) {
-      displayErrorMessage(
-        `Failed to post the status because of exception: ${error}`
-      );
-    } finally {
-      clearLastInfoMessage();
-      setIsLoading(false);
-    }
+  const listener: PostStatusView = {
+    displayErrorMessage: displayErrorMessage,
+    displayInfoMessage: displayInfoMessage,
+    clearLastInfoMessage: clearLastInfoMessage,
   };
 
-  const postStatus = async (
-    authToken: AuthToken,
-    newStatus: Status
-  ): Promise<void> => {
-    // Pause so we can see the logging out message. Remove when connected to the server
-    await new Promise((f) => setTimeout(f, 2000));
+  const [presenter] = useState(new PostStatusPresenter(listener));
 
-    // TODO: Call the server to post the status
+  const submitPost = async (event: React.MouseEvent) => {
+    presenter.submitPost(event, currentUser!, authToken!);
   };
 
   const clearPost = (event: React.MouseEvent) => {
@@ -55,7 +36,7 @@ const PostStatus = () => {
   };
 
   return (
-    <div className={isLoading ? "loading" : ""}>
+    <div className={presenter.isLoading ? "loading" : ""}>
       <form>
         <div className="form-group mb-3">
           <textarea
@@ -78,7 +59,7 @@ const PostStatus = () => {
             style={{ width: "8em" }}
             onClick={(event) => submitPost(event)}
           >
-            {isLoading ? (
+            {presenter.isLoading ? (
               <span
                 className="spinner-border spinner-border-sm"
                 role="status"
---
+++ tweeter-web/src/presenters/PostStatusPresenter.ts
@@ -0,0 +1,52 @@
+import { AuthToken, Status, User } from "tweeter-shared";
+import { StatusService } from "../model/service/StatusService";
+
+export interface PostStatusView {
+  displayInfoMessage: (
+    message: string,
+    duration: number,
+    bootstrapClasses?: string
+  ) => void;
+  displayErrorMessage: (message: string, bootstrapClasses?: string) => void;
+  clearLastInfoMessage: () => void;
+}
+
+export class PostStatusPresenter {
+  private view: PostStatusView;
+  private statusService: StatusService;
+
+  public isLoading = false;
+  public post = "";
+
+  public constructor(view: PostStatusView) {
+    this.view = view;
+    this.statusService = new StatusService();
+  }
+
+  public async submitPost(
+    event: React.MouseEvent,
+    currentUser: User,
+    authToken: AuthToken
+  ) {
+    event.preventDefault();
+
+    try {
+      this.isLoading = true;
+      this.view.displayInfoMessage("Posting status...", 0);
+
+      const status = new Status(this.post, currentUser, Date.now());
+
+      await this.statusService.postStatus(authToken, status);
+
+      this.post = "";
+      this.view.displayInfoMessage("Status posted!", 2000);
+    } catch (error) {
+      this.view.displayErrorMessage(
+        `Failed to post the status because of exception: ${error}`
+      );
+    } finally {
+      this.view.clearLastInfoMessage();
+      this.isLoading = false;
+    }
+  }
+}
