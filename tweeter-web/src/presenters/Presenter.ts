22d084f27d54ce9c43e89d6c87e11d23f445c1ac
ssummerhays <spencersummerhays3@gmail.com>
2025-02-25 11:56:26 -0700

    Created base presenter class

---
+++ tweeter-web/src/presenters/Presenter.ts
@@ -0,0 +1,37 @@
+export interface View {
+  displayErrorMessage: (message: string) => void;
+}
+
+export interface MessageView extends View {
+  displayInfoMessage: (
+    message: string,
+    duration: number,
+    bootstrapClasses?: string
+  ) => void;
+  clearLastInfoMessage: () => void;
+}
+
+export class Presenter<V extends View> {
+  private _view: V;
+
+  public constructor(view: V) {
+    this._view = view;
+  }
+
+  protected get view(): V {
+    return this._view;
+  }
+
+  public async doFailuareReportingOperation(
+    operation: () => Promise<void>,
+    operationDescription: string
+  ) {
+    try {
+      await operation();
+    } catch (error) {
+      this.view.displayErrorMessage(
+        `Failed to ${operationDescription} because of exception: ${error}`
+      );
+    }
+  }
+}
