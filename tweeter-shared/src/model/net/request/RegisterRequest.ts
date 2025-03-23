f5c1384b73924903dd943a5f0c84c135f6218037
ssummerhays <spencersummerhays3@gmail.com>
2025-03-23 15:00:25 -0600

    Created RegisterUserRequest interface

---
+++ tweeter-shared/src/model/net/request/RegisterRequest.ts
@@ -0,0 +1,8 @@
+export interface RegisterRequest {
+  firstName: string;
+  lastName: string;
+  alias: string;
+  password: string;
+  userImageBytesString: string;
+  imageFileExtension: string;
+}
