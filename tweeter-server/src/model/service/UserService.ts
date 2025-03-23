6dee68a67dcd8186c6e847448728d5355a4b96af
ssummerhays <spencersummerhays3@gmail.com>
2025-03-23 15:54:48 -0600

    Updated getIsFollowerStatus signature

--- tweeter-server/src/model/service/UserService.ts
+++ tweeter-server/src/model/service/UserService.ts
@@ -45,9 +45,9 @@ export class UserService {
   }
 
   public async getIsFollowerStatus(
-    authToken: AuthToken,
-    user: User,
-    selectedUser: User
+    token: string,
+    user: UserDto,
+    selectedUser: UserDto
   ): Promise<boolean> {
     // TODO: Replace with the result of calling server
     return FakeData.instance.isFollower();
