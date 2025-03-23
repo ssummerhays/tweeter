8a9713c9c472a85f05944161e7bbf9f3cdf9491a
ssummerhays <spencersummerhays3@gmail.com>
2025-03-23 14:59:19 -0600

    Updated LoginLambda to use new AuthResponse

--- tweeter-server/src/lambda/user/LoginUserLambda.ts
+++ tweeter-server/src/lambda/user/LoginUserLambda.ts
@@ -1,7 +1,7 @@
-import { LoginRequest, LoginResponse } from "tweeter-shared";
+import { LoginRequest, AuthResponse } from "tweeter-shared";
 import { UserService } from "../../model/service/UserService";
 
-export const handler = async (request: LoginRequest): Promise<LoginResponse> => {
+export const handler = async (request: LoginRequest): Promise<AuthResponse> => {
     const userService = new UserService();
     const [userDto, authToken] = await userService.login(request.alias, request.password);
 
