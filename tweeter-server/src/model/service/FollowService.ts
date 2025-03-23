78cc9d3c525bfd1384eaf0f2d91f0503bc830222
ssummerhays <spencersummerhays3@gmail.com>
2025-03-23 12:34:19 -0600

    Added followService for tweeter-server

---
+++ tweeter-server/src/model/service/FollowService.ts
@@ -0,0 +1,39 @@
+import { AuthToken, User, FakeData, UserDto } from "tweeter-shared";
+
+export class FollowService {
+  public async loadMoreFollowers(
+    token: string,
+    userAlias: string,
+    pageSize: number,
+    lastItem: UserDto | null
+  ): Promise<[UserDto[], boolean]> {
+    // TODO: Replace with the result of calling server
+    return this.getFakeData(lastItem, pageSize, userAlias);
+  }
+
+  public async loadMoreFollowees(
+    token: string,
+    userAlias: string,
+    pageSize: number,
+    lastItem: UserDto | null
+  ): Promise<[UserDto[], boolean]> {
+    // TODO: Replace with the result of calling server
+    return this.getFakeData(lastItem, pageSize, userAlias);
+  }
+
+  private async getFakeData(
+    lastItem: UserDto | null,
+    pageSize: number,
+    userAlias: string
+  ): Promise<[UserDto[], boolean]> {
+    const [items, hasMore] = FakeData.instance.getPageOfUsers(
+      User.fromDto(lastItem),
+      pageSize,
+      userAlias
+    );
+    const dtos = items.map((user) => user.dto);
+    return [dtos, hasMore];
+  }
+
+  
+}
