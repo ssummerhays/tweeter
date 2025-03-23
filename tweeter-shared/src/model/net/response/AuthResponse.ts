c5ebbae66d0b574624e2cf0f6d213fc7093e3b3e
ssummerhays <spencersummerhays3@gmail.com>
2025-03-23 14:59:36 -0600

    Changed LoginResponse to AuthResponse

--- tweeter-shared/src/model/net/response/AuthResponse.ts
+++ tweeter-shared/src/model/net/response/AuthResponse.ts
@@ -2,7 +2,7 @@ import { AuthTokenDto } from "../../dto/AuthTokenDto";
 import { UserDto } from "../../dto/UserDto";
 import { TweeterResponse } from "./TweeterResponse";
 
-export interface LoginResponse extends TweeterResponse {
-    user: UserDto;
-    authToken: AuthTokenDto;
+export interface AuthResponse extends TweeterResponse {
+  user: UserDto;
+  authToken: AuthTokenDto;
 }
