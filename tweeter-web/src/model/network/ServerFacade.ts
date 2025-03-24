f2518037792e2a6bfb07b02698c4b3a42a7ccd36
ssummerhays <spencersummerhays3@gmail.com>
2025-03-24 12:57:45 -0600

    Created updateFollowStatus for follow and unfollow endpoints

--- tweeter-web/src/model/network/ServerFacade.ts
+++ tweeter-web/src/model/network/ServerFacade.ts
@@ -4,6 +4,8 @@ import {
   PagedUserItemRequest,
   PagedUserItemResponse,
   Status,
+  TokenUserRequest,
+  UpdateFollowResponse,
   User,
   UserDto,
 } from "tweeter-shared";
@@ -11,6 +13,7 @@ import { ClientCommunicator } from "./ClientCommunicator";
 
 type UserType = "followers" | "followee";
 type StatusType = "feed" | "story";
+type UpdateType = "follow" | "unfollow";
 
 export class ServerFacade {
   private SERVER_URL = "TODO: Set this value.";
@@ -32,7 +35,7 @@ export class ServerFacade {
         ? response.items.map((dto) => User.fromDto(dto) as User)
         : null;
 
-    const userTypeString = (userType === "followers") ? "followers" : "followees";
+    const userTypeString = userType === "followers" ? "followers" : "followees";
     // Handle errors
     if (response.success) {
       if (items == null) {
@@ -56,13 +59,12 @@ export class ServerFacade {
     >(request, `/${statusType}/list`);
 
     // Convert the StatusDto array returned by ClientCommunicator to a Status array
-    const items: Status[] | null = 
-        response.success && response.items
+    const items: Status[] | null =
+      response.success && response.items
         ? response.items.map((dto) => Status.fromDto(dto) as Status)
         : null;
 
-    
-    const statusTypeString =  (statusType === "story") ? "stories" : "feeds";
+    const statusTypeString = statusType === "story" ? "stories" : "feeds";
     // Handle errors
     if (response.success) {
       if (items == null) {
@@ -70,6 +72,24 @@ export class ServerFacade {
       } else {
         return [items, response.hasMore];
       }
+    } else {
+      console.error(response);
+      throw new Error(response.message ?? "An unknown error occurred.");
+    }
+  }
+
+  public async updateFollowStatus(
+    request: TokenUserRequest,
+    updateType: UpdateType
+  ): Promise<[number, number]> {
+    const response = await this.clientCommunicator.doPost<
+      TokenUserRequest,
+      UpdateFollowResponse
+    >(request, `/user/${updateType}`);
+
+    // Handle errors
+    if (response.success) {
+      return [response.followeeCount, response.followerCount];
     } else {
       console.error(response);
       throw new Error(response.message ?? "An unknown error occurred.");
