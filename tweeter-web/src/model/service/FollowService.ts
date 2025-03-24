7b5906ac6afde36aa7009a4edf79c754624b34cc
ssummerhays <spencersummerhays3@gmail.com>
2025-03-24 14:04:27 -0600

    Updated FollowService to call server instead of FakeData

--- tweeter-web/src/model/service/FollowService.ts
+++ tweeter-web/src/model/service/FollowService.ts
@@ -1,14 +1,32 @@
-import { AuthToken, User, FakeData } from "tweeter-shared";
+import {
+  AuthToken,
+  User,
+  FakeData,
+  PagedItemRequest,
+  PagedItemResponse,
+} from "tweeter-shared";
+import { ServerFacade } from "../network/ServerFacade";
 
 export class FollowService {
+  private serverFacade: ServerFacade = new ServerFacade();
+
   public async loadMoreFollowers(
     authToken: AuthToken,
     userAlias: string,
     pageSize: number,
     lastItem: User | null
   ): Promise<[User[], boolean]> {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfUsers(lastItem, pageSize, userAlias);
+    const request: PagedItemRequest<User> = {
+      token: authToken.token,
+      userAlias: userAlias,
+      pageSize: pageSize,
+      lastItem: lastItem,
+    };
+    return await this.serverFacade.getMoreItems<
+      PagedItemRequest<User>,
+      PagedItemResponse<User>,
+      User
+    >(request, "followers");
   }
 
   public async loadMoreFollowees(
@@ -17,7 +35,16 @@ export class FollowService {
     pageSize: number,
     lastItem: User | null
   ): Promise<[User[], boolean]> {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfUsers(lastItem, pageSize, userAlias);
+    const request: PagedItemRequest<User> = {
+      token: authToken.token,
+      userAlias: userAlias,
+      pageSize: pageSize,
+      lastItem: lastItem,
+    };
+    return await this.serverFacade.getMoreItems<
+      PagedItemRequest<User>,
+      PagedItemResponse<User>,
+      User
+    >(request, "followee");
   }
 }
