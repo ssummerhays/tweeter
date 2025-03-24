2ab7096a7a7b5c4e12461ef6f9552cb22547920d
ssummerhays <spencersummerhays3@gmail.com>
2025-03-24 17:04:48 -0600

    Changed postStatus to call server facade method

--- tweeter-web/src/model/service/StatusService.ts
+++ tweeter-web/src/model/service/StatusService.ts
@@ -1,14 +1,33 @@
-import { AuthToken, Status, FakeData } from "tweeter-shared";
+import {
+  AuthToken,
+  Status,
+  PagedItemRequest,
+  PagedItemResponse,
+  PostStatusRequest,
+  StatusDto,
+} from "tweeter-shared";
+import { ServerFacade } from "../network/ServerFacade";
 
 export class StatusService {
+  private serverFacade: ServerFacade = new ServerFacade();
+
   public async loadMoreFeedItems(
     authToken: AuthToken,
     userAlias: string,
     pageSize: number,
     lastItem: Status | null
   ): Promise<[Status[], boolean]> {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
+    const request: PagedItemRequest<StatusDto> = {
+      token: authToken.token,
+      userAlias: userAlias,
+      pageSize: pageSize,
+      lastItem: lastItem?.dto ?? null,
+    };
+    return await this.serverFacade.getMoreItems<
+      PagedItemRequest<StatusDto>,
+      PagedItemResponse<StatusDto>,
+      Status
+    >(request, "feed");
   }
 
   public async loadMoreStoryItems(
@@ -17,8 +36,17 @@ export class StatusService {
     pageSize: number,
     lastItem: Status | null
   ): Promise<[Status[], boolean]> {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
+    const request: PagedItemRequest<StatusDto> = {
+      token: authToken.token,
+      userAlias: userAlias,
+      pageSize: pageSize,
+      lastItem: lastItem?.dto ?? null,
+    };
+    return await this.serverFacade.getMoreItems<
+      PagedItemRequest<StatusDto>,
+      PagedItemResponse<StatusDto>,
+      Status
+    >(request, "story");
   }
 
   public async postStatus(
@@ -28,6 +56,10 @@ export class StatusService {
     // Pause so we can see the logging out message. Remove when connected to the server
     await new Promise((f) => setTimeout(f, 2000));
 
-    // TODO: Call the server to post the status
+    const request: PostStatusRequest = {
+      token: authToken.token,
+      newStatus: newStatus.dto,
+    };
+    this.serverFacade.postStatus(request);
   }
 }
