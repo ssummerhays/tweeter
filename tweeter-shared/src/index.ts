f6968c3265b5f11281a893ace690e338529fa97d
ssummerhays <spencersummerhays3@gmail.com>
2025-03-24 12:39:03 -0600

    Updated tweeter-shared exports

--- tweeter-shared/src/index.ts
+++ tweeter-shared/src/index.ts
@@ -20,6 +20,7 @@ export type { StatusDto } from "./model/dto/StatusDto";
 //
 // Requests
 //
+export type { TweeterRequest } from "./model/net/request/TweeterRequest";
 export type { PagedUserItemRequest } from "./model/net/request/PagedUserItemRequest";
 export type { PagedStatusItemRequest } from "./model/net/request/PagedStatusItemRequest";
 export type { LoginRequest } from "./model/net/request/LoginRequest";
