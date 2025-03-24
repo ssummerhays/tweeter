e48cd291f6343030ebdc03ab699cc55fd991e051
ssummerhays <spencersummerhays3@gmail.com>
2025-03-24 17:03:36 -0600

    Added get dto and fromDto methods

--- tweeter-shared/src/model/domain/PostSegment.ts
+++ tweeter-shared/src/model/domain/PostSegment.ts
@@ -1,3 +1,5 @@
+import { PostSegmentDto } from "../dto/PostSegmentDto";
+
 export enum Type {
   text = "Text",
   alias = "Alias",
@@ -37,5 +39,23 @@ export class PostSegment {
 
   public get type(): Type {
     return this._type;
+  }
+
+  public get dto(): PostSegmentDto {
+    return {
+      text: this._text,
+      startPostion: this._startPostion,
+      endPosition: this._endPosition,
+      type: this._type,
+    };
+  }
+
+  public static fromDto(dto: PostSegmentDto): PostSegment {
+    return new PostSegment(
+      dto.text,
+      dto.startPostion,
+      dto.endPosition,
+      dto.type as Type
+    );
   }
 }
