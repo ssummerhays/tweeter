f271cfbffb67f3b4fccda225535e88c0cb927286
ssummerhays <spencersummerhays3@gmail.com>
2025-03-23 18:32:36 -0600

    Added StatusService to tweeter-server

---
+++ tweeter-server/src/model/service/StatusService.ts
@@ -0,0 +1,44 @@
+import { AuthToken, Status, FakeData, StatusDto } from "tweeter-shared";
+
+export class StatusService {
+  public async loadMoreFeedItems(
+    token: string,
+    userAlias: string,
+    pageSize: number,
+    lastItem: StatusDto | null
+  ): Promise<[StatusDto[], boolean]> {
+    // TODO: Replace with the result of calling server
+    return this.getFakeStatusData(lastItem, pageSize);
+  }
+
+  public async loadMoreStoryItems(
+    token: string,
+    userAlias: string,
+    pageSize: number,
+    lastItem: StatusDto | null
+  ): Promise<[StatusDto[], boolean]> {
+    // TODO: Replace with the result of calling server
+    return this.getFakeStatusData(lastItem, pageSize);
+  }
+
+  public async postStatus(token: string, newStatus: StatusDto): Promise<void> {
+    // Pause so we can see the logging out message. Remove when connected to the server
+    await new Promise((f) => setTimeout(f, 2000));
+
+    // TODO: Call the server to post the status
+  }
+
+  private async getFakeStatusData(
+    lastItem: StatusDto | null,
+    pageSize: number
+  ): Promise<[StatusDto[], boolean]> {
+    const [items, hasMore] = FakeData.instance.getPageOfStatuses(
+      Status.fromDto(lastItem),
+      pageSize
+    );
+
+    const dtos = items.map((status) => status.dto);
+
+    return [dtos, hasMore];
+  }
+}
