f31909ff08ddc58a6d41f4283b6f4f372f6fad5c
ssummerhays <spencersummerhays3@gmail.com>
2025-03-24 19:27:35 -0600

    Created StatusService test file and added loadMoreStoryItems test

---
+++ tweeter-web/test/model/service/StatusService.test.ts
@@ -0,0 +1,23 @@
+import { AuthToken } from "tweeter-shared";
+import { StatusService } from "../../../src/model/service/StatusService";
+
+import "isomorphic-fetch";
+
+describe("Status Service", () => {
+  const statusService = new StatusService();
+  const authToken = new AuthToken("token", 100);
+
+  it("gets a user's story pages", async () => {
+    const [statusItems, hasMore] = await statusService.loadMoreStoryItems(
+      authToken,
+      "@allen",
+      10,
+      null
+    );
+
+    expect(statusItems.length).toBe(10);
+    expect(statusItems[0].user.alias).toBe("@allen");
+
+    expect(hasMore).toBe(true);
+  });
+});
