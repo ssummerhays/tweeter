a2e44824b11c15dd65b305df0c01174da3df611f
ssummerhays <spencersummerhays3@gmail.com>
2025-04-15 11:41:38 -0600

    Removed try catch block to allow handler to throw errors

--- tweeter-server/src/lambda/status/PostUpdateFeedMessages.ts
+++ tweeter-server/src/lambda/status/PostUpdateFeedMessages.ts
@@ -6,51 +6,47 @@ const sqsClient = new SQSClient({ region: "us-west-2" });
 const followDao = new DynamoFollowDao();
 
 export const handler = async (event: any) => {
-  try {
-    for (let i = 0; i < event.Records.length; i++) {
-      const record = event.Records[i];
-      const body = record.body;
+  for (let i = 0; i < event.Records.length; i++) {
+    const record = event.Records[i];
+    const body = record.body;
 
-      const { status } = JSON.parse(body);
+    const { status } = JSON.parse(body);
 
-      let hasMore = true;
-      let lastItem = undefined;
-      while (hasMore) {
-        const startTimeMillis = new Date().getTime();
-        const followerPage = await followDao.getPageOfFollowers(
-          status.user.alias,
-          100,
-          lastItem
+    let hasMore = true;
+    let lastItem = undefined;
+    while (hasMore) {
+      const startTimeMillis = new Date().getTime();
+      const followerPage = await followDao.getPageOfFollowers(
+        status.user.alias,
+        100,
+        lastItem
+      );
+
+      hasMore = followerPage.hasMore;
+      lastItem = followerPage.lastFollowerAlias;
+
+      const followerAliases = followerPage.followerAliases;
+      console.log(followerAliases);
+
+      const messageBody = JSON.stringify({
+        status: status,
+        followerAliases: followerAliases,
+      });
+
+      const sqsCommand = new SendMessageCommand({
+        QueueUrl:
+          "https://sqs.us-west-2.amazonaws.com/034362054802/UpdateFeedQueue",
+        MessageBody: messageBody,
+      });
+
+      await sqsClient.send(sqsCommand);
+
+      const elapsedTime = new Date().getTime() - startTimeMillis;
+      if (elapsedTime < 600) {
+        await new Promise<void>((resolve) =>
+          setTimeout(resolve, 600 - elapsedTime)
         );
-
-        hasMore = followerPage.hasMore;
-        lastItem = followerPage.lastFollowerAlias;
-
-        const followerAliases = followerPage.followerAliases;
-        console.log(followerAliases);
-
-        const messageBody = JSON.stringify({
-          status: status,
-          followerAliases: followerAliases,
-        });
-
-        const sqsCommand = new SendMessageCommand({
-          QueueUrl:
-            "https://sqs.us-west-2.amazonaws.com/034362054802/UpdateFeedQueue",
-          MessageBody: messageBody,
-        });
-
-        await sqsClient.send(sqsCommand);
-
-        const elapsedTime = new Date().getTime() - startTimeMillis;
-        if (elapsedTime < 600) {
-          await new Promise<void>((resolve) =>
-            setTimeout(resolve, 600 - elapsedTime)
-          );
-        }
       }
     }
-  } catch (error) {
-    console.log("error = ", (error as Error).message);
   }
 };
