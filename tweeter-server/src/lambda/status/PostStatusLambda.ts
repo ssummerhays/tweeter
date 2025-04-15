71ce42c163b20f3fb9f35b6cd914d618a26f4fe4
ssummerhays <spencersummerhays3@gmail.com>
2025-04-15 11:38:25 -0600

    Updated PostStatusLambda to send a message to the PostUpdateFeedMessages SQS queue to asynchronously start the update feeds process

--- tweeter-server/src/lambda/status/PostStatusLambda.ts
+++ tweeter-server/src/lambda/status/PostStatusLambda.ts
@@ -1,13 +1,32 @@
 import { PostStatusRequest, TweeterResponse } from "tweeter-shared";
 import { StatusService } from "../../model/service/StatusService";
 import { DynamoDaoFactory } from "../../dao/factories/DynamoDaoFactory";
+import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
+
+const sqsClient = new SQSClient({ region: "us-west-2" });
 
 export const handler = async (
   request: PostStatusRequest
 ): Promise<TweeterResponse> => {
   const statusService = new StatusService(new DynamoDaoFactory());
-  
+
   await statusService.postStatus(request.token, request.newStatus);
+
+  const messageBody = JSON.stringify({
+    status: request.newStatus,
+  });
+
+  const sqsCommand = new SendMessageCommand({
+    QueueUrl:
+      "https://sqs.us-west-2.amazonaws.com/034362054802/PostStatusQueue",
+    MessageBody: messageBody,
+  });
+
+  try {
+    await sqsClient.send(sqsCommand);
+  } catch (error) {
+    throw new Error("[Server Error] unable to send SQS message");
+  }
 
   return {
     success: true,
